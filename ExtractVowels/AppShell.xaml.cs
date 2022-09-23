dd6c93115498271802207c09c0aa335eb0eee7e5
Athif Shaffy <athif.shaffy@gmail.com>
2022-09-23 12:59:24 -0400

    Updates for vowel related

---
+++ ExtractVowels/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtractVowels;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ ExtractVowels/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace ExtractVowels;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnVowelBtnCheck(object sender, EventArgs e)
+	{
+        ArrayList vowelListArr = new ArrayList();
+        ArrayList nonVowelListArr = new ArrayList();
+        for (int i = 0; i < entryText.Text.Length; i++)
+        {
+            //Console.WriteLine(i);
+            bool isVowel = "aeiouAEIOU".IndexOf(entryText.Text[i]) >= 0;
+            if (isVowel)
+            {
+                vowelList.Text += " " + entryText.Text[i];
+                vowelListArr.Add(entryText.Text[i]);
+            }
+            else
+            {
+                nonVowelList.Text += " " + entryText.Text[i];
+                nonVowelListArr.Add(entryText.Text[i]);
+            }
+        }
+    }
+}
+
+
